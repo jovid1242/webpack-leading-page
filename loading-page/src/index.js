596d53ccdd9bbbeba656e3b1911e06f69762556e
jovid1242 <jovid1242@gmail.com>
2021-03-30 13:57:10 +0500

    header

--- loading-page/src/index.js
+++ loading-page/src/index.js
@@ -1,2 +1,12 @@
 import './scss/index.scss'
 import 'bootstrap/dist/css/bootstrap.css';
+
+window.onscroll = function (e) {
+    const nav = document.querySelector('.navbar')
+    if (window.scrollY > 0) {
+        nav.classList.add('affix');
+    }
+    if (window.scrollY === 0) {
+        nav.classList.remove('affix')
+    }
+};
